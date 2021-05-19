4ef04cf57648d24b94906c0fc82cf2f9f573b0d7
shaheenshanavaz <84439067+shaheenshanavaz@users.noreply.github.com>
2021-05-19 21:51:05 +0530

    Add files via upload

---
+++ QRcode_generationproject.py
@@ -0,0 +1,11 @@
+import qrcode,image
+qr = qrcode.QRCode(
+    version = 15,
+    box_size = 10,
+    border = 5
+)
+data = 'https://www.hackerrank.com/domains/python'
+qr.add_data(data)
+qr.make(fit = True)
+img = qr.make_image(fill_color = 'black',back_color = 'white')
+img.save('test.png')
