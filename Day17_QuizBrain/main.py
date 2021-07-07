7bd2b102c7aae119d69bcdd4e8ef841faacc0ff5
shaheenshanavaz <84439067+shaheenshanavaz@users.noreply.github.com>
2021-07-07 21:30:27 +0530

    Add files via upload

---
+++ Day17_QuizBrain/main.py
@@ -0,0 +1,17 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've have completed your quiz")
+print(f"You're final score is: {quiz.score}/{quiz.question_number}")
