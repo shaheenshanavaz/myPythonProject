b97835d5bf32824b3cfbbccca619a10c1bb1830f
shaheenshanavaz <30301206+anupbdn@users.noreply.github.com>
2021-08-04 23:32:19 +0530

    my project

--- example_tkinker.py
+++ example_tkinker.py
@@ -1,6 +1,6 @@
 from tkinter import *
 
-#Creating a new window and configurations
+#Check Creating a new window and configurations
 window = Tk()
 window.title("Widget Examples")
 window.minsize(width=500, height=500)
