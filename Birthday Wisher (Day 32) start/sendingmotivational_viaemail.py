88211a0a587c708186b2344a48fd88d9ca029522
shaheenshanavaz <84439067+shaheenshanavaz@users.noreply.github.com>
2022-03-20 09:25:10 +0530

    Add files via upload

---
+++ Birthday Wisher (Day 32) start/sendingmotivational_viaemail.py
@@ -0,0 +1,20 @@
+import smtplib
+import datetime as dt
+import random
+
+
+user = "shaheenpython@gmail.com"
+password = "asdf@1234"
+quote = ""
+with open("quotes.txt") as file:
+   line = file.readlines()
+   quote=random.choice(line)
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user= user, password = password)
+    recipients = ["shaheen.shanavaz94@gmail.com", "anupbdn@gmail.com"]
+    connection.sendmail(from_addr=user,
+                        to_addrs=", ".join(recipients),
+                        msg=f"Subject: Today's Positivity Quotes\n\n {quote} ")
+    now = dt.datetime.now()
+    # date_to_send = dt.datetime(year=2022,month=3,day=5)
